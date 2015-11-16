55598bf650094a474cd77c846835824a49df681b
Eric Stephenson <ericb.stephenson@gmail.com>
2015-11-16 14:51:56 -0800

    Full app with CSS

---
+++ js/controllers/HomeController.js
@@ -0,0 +1,23 @@
+app.controller('HomeController', [
+  '$scope', 
+  'suggestions', 
+
+  function($scope, suggestions) {
+  $scope.posts = suggestions.posts;
+
+  $scope.addSuggestion = function() {
+  	if (!$scope.title || $scope.title === "") {
+  	  return;
+  	}
+  	$scope.posts.push({
+  		title: $scope.title,
+  		upvotes: 0,
+  		comments: []
+  	});
+  	$scope.title = '';
+  };
+  
+  $scope.upVote = function(post) {
+  	$scope.posts[post].upvotes += 1;
+  };
+}]);
---
+++ js/controllers/SuggestionController.js
@@ -0,0 +1,18 @@
+app.controller('SuggestionController', ['$scope', '$routeParams', 'suggestions', function($scope, $routeParams, suggestions) {
+	suggestions.success(function(data) {
+		$scope.post = suggestions.posts[$routeParams.id];
+		$scope.addComment = function() {
+			if(!$scope.post.comment || $scope.post.comment === "") {
+				return;
+			}
+			$scope.post.comment.push({
+				body: '',
+				upvotes: 0
+			});
+			$scope.post.comment = '';
+		};
+		$scope.upVoteComment = function(comment) {
+			$scope.post[comment].upvotes += 1; 
+		};
+	});
+}]);
